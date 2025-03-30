c5c86a5c03d50d51b0c1b38111d76aeae4b196b2
Jaime E. Osorio Salazar <jaimesass12@gmail.com>
2025-03-30 15:20:07 -0500

    Some changes about dark mode button

--- js/dark.js
+++ js/dark.js
@@ -1,5 +1,5 @@
 document.getElementById("toggle-dark-mode").addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
     const isDarkMode = document.body.classList.contains("dark-mode");
-    this.textContent = isDarkMode ? "Modo Claro" : "Modo Oscuro";
+    this.textContent = isDarkMode ? "L" : "D";
   });
